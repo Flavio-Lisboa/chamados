5b025355e331dad14aa1717bddcd00fc871cf1a4
Joao-Alves10 <joao.alvessilva1003@gmail.com>
2023-11-20 19:40:55 -0300

    rotas adicionadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { OpcoesComponent } from './opcoes/opcoes.component';
+import { AberturaChamadoComponent } from './abertura-chamado/abertura-chamado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'opcoes', component: OpcoesComponent},
+  {path: 'chamados', component: AberturaChamadoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
